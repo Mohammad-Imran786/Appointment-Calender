3003941586d32829c93e0abc6c332b97631330f1
Mohammad Imran <786.meimran@gmail.com>
2025-07-14 10:35:52 +0530

    switch to default base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base:'/Appointment-Calendar/',
   plugins: [
     tailwindcss(),
     react(),
